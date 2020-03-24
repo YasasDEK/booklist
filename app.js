f29fac70a4d061fb43e97b3f1cb1ff3f85a4def8
YasasDEK <ydilshan.ek@gmail.com>
2020-03-24 17:27:57 +0530

    final site

--- app.js
+++ app.js
@@ -10,20 +10,20 @@ class Book {
 //UI class : handle UI tasks
 class UI {
     static displayBooks() {
-        const StoredBooks = [
-            {
-                title: 'Book one',
-                author: 'William',
-                isbn: '123456'
-            },
-            {
-                title: 'Book two',
-                author: 'James',
-                isbn: '123457'
-            }
-        ];
-
-        const books = StoredBooks;
+        const books = Store.getBooks();
+        // const StoredBooks = [
+        //     {
+        //         title: 'Book one',
+        //         author: 'William',
+        //         isbn: '123456'
+        //     },
+        //     {
+        //         title: 'Book two',
+        //         author: 'James',
+        //         isbn: '123457'
+        //     }
+        // ];
+        // const books = StoredBooks;
 
         books.forEach((book) => UI.addBookToList(book));
     }
@@ -44,9 +44,20 @@ class UI {
     }
 
     static deleteBook(el) {
-        if(el.classList.contains('delete')) {
+        if (el.classList.contains('delete')) {
             el.parentElement.parentElement.remove();
-        } 
+        }
+    }
+
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+        //vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
     }
 
     static clearFields() {
@@ -54,6 +65,39 @@ class UI {
         document.querySelector('#author').value = '';
         document.querySelector('#isbn').value = '';
     }
+}
+
+//store class: handles storage
+class Store {
+    static getBooks() {
+        let books;
+        if (localStorage.getItem('books') === null) {
+            books = [];
+        }
+        else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+        books.forEach((book, index) => {
+            if (book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+
+    }
+
 }
 
 //Event : display books
@@ -69,20 +113,38 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    //Instatiate book
-    const book = new Book(title, author, isbn);
-    console.log(book);
+    //validte
+    if (title === '' || author === '' || isbn === '') {
+        UI.showAlert('please fill in all fields', 'danger')
+    }
+    else {
+        //Instatiate book
+        const book = new Book(title, author, isbn);
+        console.log(book);
 
-    //Add book to UI
-    UI.addBookToList(book);
+        //Add book to UI
+        UI.addBookToList(book);
 
-    //clear field
-    UI.clearFields();
+        //add book to store
+        Store.addBook(book);
 
+        //show success message
+        UI.showAlert('Book added', 'success');
+
+        //clear field
+        UI.clearFields();
+    }
 });
 
 //Event : remove a book
 document.querySelector('#book-list').addEventListener('click', (e) => {
     console.log(e.target);
+    //remove a book from UI
     UI.deleteBook(e.target);
+
+    //remove a book from store
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+    //show delete message
+    UI.showAlert('Book deleted', 'danger');
 });
